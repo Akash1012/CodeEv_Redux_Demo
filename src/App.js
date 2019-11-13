8d3c35aeff00f40764e04a7d83a9de278b38adee
Akash Gupta <akash@totient.co.in>
2019-11-13 14:13:24 +0530

    Adding Hooks With Redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import CakeContainer from "./Components/CakeContainer";
+import HooksCakeContainer from "./Components/HooksCakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
